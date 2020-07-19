030f753ddc3a795d4c4024c4be8cf481d09c6037
Prabin <psmagar123451@gmail.com>
2020-07-19 13:00:07 +0545

    files

---
+++ script.js
@@ -0,0 +1,15 @@
+const showBtn = document.querySelector('.navBtn');
+const topNav = document.querySelector('.top-nav');
+
+showBtn.addEventListener('click', function(){
+    if(topNav.classList.contains('showNav')){
+        topNav.classList.remove('showNav');
+        showBtn.innerHTML = '<i class = "fas fa-bars"></i>';
+    } else {
+        topNav.classList.add('showNav');
+        showBtn.innerHTML = '<i class = "fas fa-times"></i>';
+    }
+});
+
+/* Lightbox */
+var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
